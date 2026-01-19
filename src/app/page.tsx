9ddf459e86e4996474c8c8d6e61c8aaeed98b7d4
gitdevjin <dev.hjshin@gmail.com>
2026-01-19 18:21:37 -0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+import { getUsers } from "@/api/get-users";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const users = await getUsers();
+  console.log(users);
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <div>
+        {users.map((user) => (
+          <div>{user.name}</div>
+        ))}
+      </div>
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
